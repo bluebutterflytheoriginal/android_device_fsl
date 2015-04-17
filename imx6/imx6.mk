5c9b970b702f866287d7d16420f6d92bbbe1dbbf
Gary Bisson <gary.bisson@boundarydevices.com>
2015-04-17 11:20:38 +0200

    imx*.mk: use the no_telephony product
    Use the no_telephony product as base system, to avoid installing unused
    packages. This also fixes, as a consequence, the "Unfortunately,
    Messaging has stopped" problem.
    
    Signed-off-by: Diego Rondini <diego.rondini@kynetics.it>
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- imx6/imx6.mk
+++ imx6/imx6.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
 ifeq ($(PRODUCT_IMX_CAR),true)
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 endif
