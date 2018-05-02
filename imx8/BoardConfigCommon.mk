084befbed55a045932a11c3dad460b34126877ee
Gary Bisson <gary.bisson@boundarydevices.com>
2018-05-02 14:26:46 +0200

    imx8: make A/B OTA scheme optional depending on AB_OTA_UPDATER
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- imx8/BoardConfigCommon.mk
+++ imx8/BoardConfigCommon.mk
@@ -66,11 +66,13 @@ BOARD_GPU_DRIVERS := vivante
 # comment out for 1g/3g space split
 # TARGET_KERNEL_2G := true
 
+ifneq ($(AB_OTA_UPDATER),false)
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := boot system vendor vbmeta
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := true
+endif
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 
